efcc49a330c24f28090aaa7fb2f5c1a503ac8c25
Konrad Czerepak <kczerepakto@gmail.com>
2023-09-30 08:46:10 +0000

    Update collector.sh

--- collector.sh
+++ collector.sh
@@ -9,5 +9,5 @@ pip3 install opentelemetry-exporter-otlp-proto-grpc --user
 pip3 install psutil --user
 
 rm -f scrap-metrics.py
-wget https://gitlab.com/lwronski/observability/-/raw/bug/user-out-of-scope/scrap-metrics.py
+wget https://gitlab.com/lwronski/observability/-/raw/main/scrap-metrics.py
 python3 -u scrap-metrics.py $COLLECTOR_ENDPOINT &>scrapping_logs.txt
