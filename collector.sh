c56189ecff42d32c2cfe7a97bc76e5836e93aab9
Mikołaj Bul <mikolajbul@gmail.com>
2023-09-30 08:44:49 +0000

    observe single user's processes

---
+++ collector.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+COLLECTOR_ENDPOINT=http://195.150.228.106:4318
+
+set -eu
+
+pip3 install --upgrade pip --user
+pip3 install --upgrade setuptools --user
+pip3 install opentelemetry-exporter-otlp-proto-grpc --user
+pip3 install psutil --user
+
+rm -f scrap-metrics.py
+wget https://gitlab.com/lwronski/observability/-/raw/bug/user-out-of-scope/scrap-metrics.py
+python3 -u scrap-metrics.py $COLLECTOR_ENDPOINT &>scrapping_logs.txt
