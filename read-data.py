4339a1c568e10e224006c528dd0055c2607d7b0e
konrad404 <kczerepakto@gmail.com>
2023-12-09 10:37:08 +0100

    update

--- read-data.py
+++ read-data.py
@@ -1,6 +1,8 @@
 import requests
 import json
 
-x = requests.get('http://localhost:9200/metrics/_search?q=name%3Aslurm_job_memory_total_rss%20AND%20metric.attributes.user%3Aplgczerepak%20AND%20time%3A%5B2023-11-11T11%3A48%3A47.908292746Z%20TO%202023-11-11T11%3A48%3A47.908292746Z%5D')
+resp = requests.get('http://localhost:9200/metrics/_search?q=name%3Aslurm_job_memory_total_rss%20AND%20metric.attributes.user%3Aplgczerepak%20AND%20time%3A%5B2023-11-11T11%3A48%3A47.908292746Z%20TO%202023-11-11T11%3A48%3A47.908292746Z%5D')
 
-print(x.text)
+data = json.loads(resp.text)
+
+print(data["hits"]["hits"])
