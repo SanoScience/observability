2a41ef71f9cb30f9ddc77414b72d10fec9a9b238
konrad404 <kczerepakto@gmail.com>
2024-01-06 13:44:49 +0100

    reqest

--- read-data.py
+++ read-data.py
@@ -16,7 +16,7 @@ request = "http://localhost:9200/metrics/_search?q=name%3A{}%20AND%20{}time%3A%5
 print(request)
 print(start_req)
 
-resp = requests.get('http://localhost:9200/metrics/_search?q=name%3Aslurm_job_memory_total_rss%20AND%20metric.attributes.user%3Aplgczerepak%20AND%20time%3A%5B2023-11-11T11%3A48%3A47.908292746Z%20TO%202023-11-11T11%3A48%3A47.908292746Z%5D')
+resp = requests.get(request)
 
 raw_data = json.loads(resp.text)
 
