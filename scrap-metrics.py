f377975eb6873d97018ba0e7eb7030ec402ffce2
konrad404 <72197032+konrad404@users.noreply.github.com>
2024-01-25 12:08:38 +0100

    Update scrap-metrics.py

--- scrap-metrics.py
+++ scrap-metrics.py
@@ -41,7 +41,7 @@ print(args.pipeline_identifier)
 pipeline_id = re.search("\d+$", args.pipeline_identifier)
 print(pipeline_id)
 if pipeline_id is not None:
-    pipeline_id = pipeline_id.start()
+    pipeline_id = pipeline_id.group(0)
 print(pipeline_id)
 
 resource = Resource(attributes={
