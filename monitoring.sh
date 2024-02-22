bab4417100fd41d63678dae6b7cd17b0d20287af
konrad404 <kczerepakto@gmail.com>
2024-02-22 14:56:49 +0100

    fix

--- monitoring.sh
+++ monitoring.sh
@@ -5,10 +5,9 @@ COLLECTOR_ENDPOINT=http://195.150.228.106:4318
 
 function setup_conda() {
     module load miniconda3
-    pip3 install --upgrade pip --user
-    pip3 install --upgrade setuptools --user
 
     if [ ! -d $ENV_PATH ]; then
+        
         mkdir -p $SCRATCH/.conda
         conda config --add pkgs_dirs $SCRATCH/.conda
         conda env create --prefix $ENV_PATH --file $1
@@ -16,6 +15,12 @@ function setup_conda() {
     
     conda config --set auto_activate_base false
     source activate $ENV_PATH
+
+    pip3 install --upgrade pip --user
+    pip3 install --upgrade setuptools --user
+    pip3 install opentelemetry-exporter-otlp-proto-grpc --user
+    pip3 install psutil --user
+    pip3 install argparse --user
 }
 
 function install_packages() {
