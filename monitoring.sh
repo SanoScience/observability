97d0f26dc2269b4b375c670c823d637e423fbb13
konrad404 <kczerepakto@gmail.com>
2024-02-26 17:34:30 +0100

    lock file fix

--- monitoring.sh
+++ monitoring.sh
@@ -4,24 +4,20 @@ ENV_PATH=$SCRATCH/mee_monitoring/env
 COLLECTOR_ENDPOINT=http://195.150.228.106:4318
 
 function setup_conda() {
+    module load miniconda3
     LOCK_FILE=$SCRATCH/mee_monitoring/setup_conda.lock
+
+    if [ ! -d $ENV_PATH ]; then
+        
+        mkdir -p $SCRATCH/.conda
+        conda config --add pkgs_dirs $SCRATCH/.conda
+        conda env create --prefix $ENV_PATH --file $1
+    fi
 
     if [ ! -f $LOCK_FILE ]; then
         
         touch $LOCK_FILE
         flock -x $LOCK_FILE
-    
-        module load miniconda3
-
-        if [ ! -d $ENV_PATH ]; then
-            
-            mkdir -p $SCRATCH/.conda
-            conda config --add pkgs_dirs $SCRATCH/.conda
-            conda env create --prefix $ENV_PATH --file $1
-        fi
-        
-        conda config --set auto_activate_base false
-        source activate $ENV_PATH
 
         pip3 install --upgrade pip --user
         pip3 install --upgrade setuptools --user
@@ -30,8 +26,13 @@ function setup_conda() {
         pip3 install argparse --user
 
         flock -u $LOCK_FILE
-    else
-        flock -n $LOCK_FILE
+    elif
+        flock -x $LOCK_FILE
+
+        conda config --set auto_activate_base false
+        source activate $ENV_PATH
+        
+        flock -u $LOCK_FILE
     fi
 }
 
