c1c673bce42c8417d4d20998dc7aecaa327cdde9
konrad404 <kczerepakto@gmail.com>
2024-02-29 12:43:45 +0100

    lock file fix

--- monitoring.sh
+++ monitoring.sh
@@ -12,48 +12,47 @@ function setup_env(){
     module load miniconda3
 
     if [ ! -d $ENV_PATH ]; then
-        
         mkdir -p $SCRATCH/.conda
         conda config --add pkgs_dirs $SCRATCH/.conda
         conda env create --prefix $ENV_PATH --file $1
+    fi
 
-        conda config --set auto_activate_base false
-        source activate $ENV_PATH
+    conda config --set auto_activate_base false
+    source activate $ENV_PATH
 
-        pip3 install --upgrade pip --user
-        pip3 install --upgrade setuptools --user
+    pip3 install --upgrade pip --user
+    pip3 install --upgrade setuptools --user
 
 
-        if is_package_installed opentelemetry-exporter-otlp-proto-grpc; then
-            echo "opentelemetry-exporter-otlp-proto-grpc is installed"
-        else
-            pip3 install opentelemetry-exporter-otlp-proto-grpc --user
-        fi
+    if is_package_installed opentelemetry-exporter-otlp-proto-grpc; then
+        echo "opentelemetry-exporter-otlp-proto-grpc is installed"
+    else
+        pip3 install opentelemetry-exporter-otlp-proto-grpc --user
+    fi
 
-        if is_package_installed psutil; then
-            echo "psutil is installed"
-        else
-            pip3 install psutil --user
-        fi
+    if is_package_installed psutil; then
+        echo "psutil is installed"
+    else
+        pip3 install psutil --user
+    fi
 
-        if is_package_installed argparse; then
-            echo "argparse is installed"
-        else
-            pip3 install argparse --user
-        fi
+    if is_package_installed argparse; then
+        echo "argparse is installed"
+    else
+        pip3 install argparse --user
     fi
 }
 
 function setup_conda() {
-    module load miniconda3
+    # module load miniconda3
     LOCK_FILE=$SCRATCH/mee_monitoring/setup_conda.lock
 
-    if [ ! -d $ENV_PATH ]; then
+    # if [ ! -d $ENV_PATH ]; then
         
-        mkdir -p $SCRATCH/.conda
-        conda config --add pkgs_dirs $SCRATCH/.conda
-        conda env create --prefix $ENV_PATH --file $1
-    fi
+    #     mkdir -p $SCRATCH/.conda
+    #     conda config --add pkgs_dirs $SCRATCH/.conda
+    #     conda env create --prefix $ENV_PATH --file $1
+    # fi
 
     if [ ! -f "$LOCK_FILE" ]; then
         
