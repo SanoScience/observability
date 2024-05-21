b14699fffbc9db5fa872ad512c9d498f8739f3c3
konrad404 <kczerepakto@gmail.com>
2024-05-21 10:06:13 +0200

    cpu_stat debug

--- scrap-metrics.py
+++ scrap-metrics.py
@@ -103,8 +103,9 @@ uid = get_own_uid()
 user = get_username(uid)
 wait_for_job_start(uid, job)
 mem_path = '/sys/fs/cgroup/memory/slurm/uid_{}/job_{}/'.format(uid, job)
+cpu_path = '/sys/fs/cgroup/cpu/slurm/uid_{}/job_{}/'.format(uid, job)
 
-result = subprocess.run(['ls', mem_path], capture_output=True, text=True)
+result = subprocess.run(['ls', cpu_path], capture_output=True, text=True)
 print(result.stdout)
 
 base_metric_labels = {
