baf8a22698f9303c87779e379e0406ed763f56e9
konrad404 <kczerepakto@gmail.com>
2024-09-26 09:53:25 +0200

    debug

--- monitoring.sh
+++ monitoring.sh
@@ -67,6 +67,7 @@ function run_monitoring() {
     rm -f scrap-metrics.py
 
     wget -q https://raw.githubusercontent.com/SanoScience/observability/develop/scrap-metrics.py
+    echo "start monitoring"
     conda run -n $ENV_NAME --live-stream python3 -u scrap-metrics.py --collector $COLLECTOR_ENDPOINT $USER_ARGS > scrapping_logs.txt 2>&1
 
 }
